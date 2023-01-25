261c6ae894ba95a428bb910606fd2d4de4a37e59
Cristian Fleita <cristianagustinfleita@gmail.com>
2023-01-25 10:27:53 -0300

    Added drawing the bounding boxes in the images

--- HogDetection/main.py
+++ HogDetection/main.py
@@ -2,8 +2,24 @@ from hog_detection import HogDetector
 from image_manager import ImageManager
 import json
 import os
+import cv2
 
-DIRECTORY_PATH = "../Images/Experiment/SET3"
+# DIRECTORY_PATH = "../Images/Experiment/SET3"
+DIRECTORY_PATH = "../Images/Test"
+
+def show_bounding_boxes(image_manager, image, image_bounding_boxes):
+    for bounding_boxes in image_bounding_boxes:
+        x = bounding_boxes["x"]
+        y = bounding_boxes["y"]
+        w = bounding_boxes["width"]
+        h = bounding_boxes["height"]
+
+        cv2.rectangle(image.get("image"), 
+                        (x, y),  
+                        (x + w, y + h),  
+                        (0, 0, 255), 
+                        4)
+        image_manager.show_image(image.get("image"))
 
 if __name__== "__main__":
     detector = HogDetector()
@@ -21,6 +37,8 @@ if __name__== "__main__":
         detections["file_name"] = image.get("file_name")
         detections["bounding_boxes"] = image_bounding_boxes
         matrix_detection.append(detections.copy())
+
+        # show_bounding_boxes(image_manager, image, image_bounding_boxes)
     
     json_object = json.dumps( matrix_detection, indent = 4 )
     folder_name = os.path.basename(DIRECTORY_PATH).split('/')[-1]
