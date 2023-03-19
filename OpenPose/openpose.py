170f994974a8d84a3fa9aff28a4962b84ef84ef1
“CristianFleita” <cristianagustinfleita@gmail.com>
2023-03-19 19:56:00 -0300

    Added created a config file for openPose Added moved openPose variables definition to body25Config.py

--- OpenPose/openpose.py
+++ OpenPose/openpose.py
@@ -3,6 +3,7 @@ import json
 import time
 import numpy as np
 import argparse
+import body25Config
 
 parser = argparse.ArgumentParser(description='Run keypoint detection')
 parser.add_argument("--device", default="cpu", help="Device to inference on")
@@ -13,30 +14,11 @@ class OpenPoseDetector:
     def __init__(self, proto_file, weights_file):
         self.proto_file = proto_file
         self.weights_file = weights_file
-        self.nPoints = 25
-        self.keypointsMapping = ["Nose", "Neck", "RShoulder", "RElbow", "RWrist", "LShoulder", "LElbow", "LWrist",
-                                 "MidHip", "RHip",
-                                 "RKnee", "RAnkle", "LHip", "LKnee", "LAnkle", "REye", "LEye", "REar", "LEar",
-                                 "LBigToe",
-                                 "LSmallToe", "LHeel", "RBigToe", "RSmallToe", "RHeel"]
-        self.POSE_PAIRS = [[1, 8], [1, 2], [1, 5], [2, 3], [3, 4], [5, 6], [6, 7],
-                           [8, 9], [9, 10], [10, 11], [8, 12], [12, 13], [13, 14],
-                           [1, 0], [0, 15], [15, 17], [0, 16], [16, 18], [2, 17], [5, 18],
-                           [14, 19], [19, 20], [14, 21], [11, 22], [22, 23], [11, 24]]
-        self.mapIdx = [[26, 27], [40, 41], [48, 49], [42, 43], [44, 45], [50, 51],
-                       [52, 53], [32, 33], [28, 29], [30, 31], [34, 35], [36, 37],
-                       [38, 39], [56, 57], [58, 59], [62, 63], [60, 61], [64, 65],
-                       [46, 47], [54, 55], [66, 67], [68, 69], [70, 71], [72, 73],
-                       [74, 75], [76, 77]]
-        self.colors = [[255, 0, 0], [255, 85, 0], [255, 170, 0],
-                       [255, 255, 0], [170, 255, 0], [85, 255, 0],
-                       [0, 255, 0], [0, 255, 85], [0, 255, 170],
-                       [0, 255, 255], [0, 170, 255], [0, 85, 255],
-                       [0, 0, 255], [85, 0, 255], [170, 0, 255],
-                       [255, 0, 255], [255, 0, 170], [255, 0, 85],
-                       [255, 170, 85], [255, 170, 170], [255, 170, 255],
-                       [255, 85, 85], [255, 85, 170], [255, 85, 255],
-                       [170, 170, 170]]
+        self.nPoints = body25Config.KEY_POINTS_NUMBER
+        self.keypointsMapping = body25Config.KEY_POINTS_MAPPING
+        self.POSE_PAIRS = body25Config.POSE_PAIRS
+        self.mapIdx = body25Config.MAP_IDX
+        self.colors = body25Config.KEY_POINTS_COLORS
 
     def getKeypoints(self, probMap, threshold=0.1):
         mapSmooth = cv2.GaussianBlur(probMap, (3, 3), 0, 0)
